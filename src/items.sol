9356e844b71c7c2b9f17f0adcbeca7f2e8742b43
Nikolai Mushegian <nikolai@dapphub.com>
2018-01-13 14:00:30 -0500

    gpl3

--- src/items.sol
+++ src/items.sol
@@ -1,3 +1,24 @@
+/// items.sol - like DSToken, but non-fungible things identified by number
+
+// Copyright (C) 2016, 2017, 2018  DappHub, LLC
+ 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.4.19;
+
+import 'ds-auth/auth.sol';
+
 contract DSItems is DSAuth {
     mapping( uint256 => address )  public handler;
     mapping( address =>
@@ -16,7 +37,7 @@ contract DSItems is DSAuth {
         handler[obj] == msg.sender;
     }
     function move(uint256 obj, address from, address to) {
-        require( msg.sender == handler[obj] || relies[msg.sender][this]);
+        require( msg.sender == handler[obj] || relies[from][this]);
         handler[obj] = to;
     }
     function pull(uint256 obj, address from) {
