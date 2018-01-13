956c3b8ffb06dd63d0b0e0c75374c0adc8df3a87
Nikolai Mushegian <nikolai@dapphub.com>
2018-01-13 13:55:01 -0500

    sketch items

---
+++ src/items.sol
@@ -0,0 +1,36 @@
+
+
+contract DSItems is DSAuth {
+    mapping( uint256 => address )  public handler;
+    mapping( address =>
+        mapping( address => bool)  public relies; // hodler -> mover -> ok
+
+    function init(uint256 obj, address where)
+        auth
+    {
+        require( handler[obj] == 0x0 );
+        handler[obj] = where;
+    }
+    function init(uint256 obj)
+        auth
+    {
+        require( handler[obj] == 0x0 );
+        handler[obj] == msg.sender;
+    }
+    function move(uint256 obj, address from, address to) {
+        require( msg.sender == handler[obj] || relies[msg.sender][this]);
+        handler[obj] = to;
+    }
+    function pull(uint256 obj, address from) {
+        move(obj, from, this);
+    }
+    function push(uint256 obj, address to) {
+        move(obj, this, to);
+    }
+    function rely(address whom) {
+        relies[msg.sender][whom] = true;
+    }
+    function deny(address whom) {
+        relies[msg.sender][whom] = false;
+    }
+}
